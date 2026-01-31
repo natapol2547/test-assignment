44a3565597863ddeb9dd9b68534dbd74f430cd89
Tawan Sunflower <96534097+natapol2547@users.noreply.github.com>
2026-01-31 19:23:00 +0700

    handle 9 and 4

--- main.ts
+++ main.ts
@@ -1,10 +1,16 @@
 const roman_numerals = [
     { value: 1000, symbol: "M" },
+    { value: 900, symbol: "CM" },
     { value: 500, symbol: "D" },
+    { value: 400, symbol: "CD" },
     { value: 100, symbol: "C" },
+    { value: 90, symbol: "XC" },
     { value: 50, symbol: "L" },
+    { value: 40, symbol: "XL" },
     { value: 10, symbol: "X" },
+    { value: 9, symbol: "IX" },
     { value: 5, symbol: "V" },
+    { value: 4, symbol: "IV" },
     { value: 1, symbol: "I" },
 ]
 
@@ -19,4 +25,4 @@ function number_to_roman(num: number): string {
     return result;
 }
 
-console.log(number_to_roman(1903));
+console.log(number_to_roman(1944));
