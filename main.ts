d260707be6854c91d96cc7b2b389c76f17ee4b15
Tawan Sunflower <96534097+natapol2547@users.noreply.github.com>
2026-01-31 19:20:31 +0700

    define roman numerals

--- main.ts
+++ main.ts
@@ -1,1 +1,16 @@
-console.log("Hello Cleverse");
+const roman_numerals = [
+    { value: 1000, symbol: "M" },
+    { value: 500, symbol: "D" },
+    { value: 100, symbol: "C" },
+    { value: 50, symbol: "L" },
+    { value: 10, symbol: "X" },
+    { value: 5, symbol: "V" },
+    { value: 1, symbol: "I" },
+]
+
+function number_to_roman(num: number): string {
+    let result = "";
+    return result;
+}
+
+console.log(number_to_roman(1903));
